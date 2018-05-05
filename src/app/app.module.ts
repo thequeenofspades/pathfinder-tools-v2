45cfdaa773f40efb75c94c0ae6eb0d77c06a2f92
Hana Lee <leehana@stanford.edu>
2018-05-04 19:11:45 -0700

    deploying to pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InitiativeComponent } from './initiative/initiative.component';
+import { PlayersComponent } from './initiative/players/players.component';
+import { MonstersComponent } from './initiative/monsters/monsters.component';
+import { InitiativeOrderComponent } from './initiative/initiative-order/initiative-order.component';
+import { PlayerFormComponent } from './initiative/players/player-form/player-form.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MonsterFormComponent } from './initiative/monsters/monster-form/monster-form.component';
+import { DamageFormComponent } from './initiative/initiative-order/damage-form.component';
+import { ConditionFormComponent } from './initiative/initiative-order/condition-form.component';
+import { InitiativeFormComponent } from './initiative/players/initiative-form.component';
+import { NoteFormComponent } from './initiative/initiative-order/note-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    InitiativeComponent,
+    PlayersComponent,
+    MonstersComponent,
+    InitiativeOrderComponent,
+    PlayerFormComponent,
+    MessagesComponent,
+    MonsterFormComponent,
+    DamageFormComponent,
+    ConditionFormComponent,
+    InitiativeFormComponent,
+    NoteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
