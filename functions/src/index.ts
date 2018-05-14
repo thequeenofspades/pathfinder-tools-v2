0690d41e663afc8cf53ab433999f6e1bca47d84e
Hana Lee <leehana@stanford.edu>
2018-05-13 23:40:46 -0700

    added cloud functions

---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.deleteOldSessions = functions.firestore
+	.document('sessions/{sessionId}')
+	.onCreate((snap, context) => {
+		let cutoff = Date.now();
+		cutoff -= 30 * 24 * 60 * 60 * 1000;
+		return admin.firestore().collection('sessions')
+			.where('timestamp', '<', cutoff)
+			.get()
+			.then(qs => {
+				qs.forEach(doc => {
+					return admin.firestore().collection('sessions')
+						.doc(doc.id)
+						.delete();
+				});
+			}).catch(err => console.log(err));
+	});
