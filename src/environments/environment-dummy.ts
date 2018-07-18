887ab2089aa3efe3e1f7ad84e55bea9b6f188ceb
Hana Lee <leehana@stanford.edu>
2018-07-18 14:15:09 -0700

    Added environment without API key

---
+++ src/environments/environment-dummy.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: "",
+		authDomain: "pf-tools.firebaseapp.com",
+		databaseURL: "https://pf-tools.firebaseio.com",
+		projectId: "pf-tools",
+		storageBucket: "pf-tools.appspot.com",
+		messagingSenderId: "95343487279"
+	}
+};
